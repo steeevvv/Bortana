56ab4d6a8776883705c6bbf2a337f973d38b1909
steeevvv <stevesame2040@gmail.com>
2021-09-07 13:32:27 +0200

    frontend integration

--- bot.py
+++ bot.py
@@ -1,32 +1,17 @@
 from rivescript import RiveScript
 
-# initializing rivescript object + Arabic Language support
 bot = RiveScript(utf8=True)
 
-# load directory of the brain file
 bot.load_directory("brain")
 bot.sort_replies()
 
 
-'''
-# bot reply validation function
 def chat(message):
     if message == "":
-        return -1, "No message found!"
+        return -1, "No message found "
     else:
-        response = bot.reply("user", message)
-        if response:
-            return 0, response
-        else:
-            return -1, "No Response found!!"
-
-'''
-
-
-while True: 
-    msg = str(input("User: "))
-    reply = str(bot.reply('localuser', msg))
-    if msg == "quit":
-        break
+        responce = bot.reply("user", message)
+    if responce:
+        return 0, responce
     else:
-        print("Bot: " + reply)
+        return -1, "No responce found"
--- main.py
+++ main.py
@@ -1,42 +1,23 @@
-from flask import Flask, render_template, request, jsonify, Response
-
+from flask import Flask, request, jsonify
 import bot
-import utils
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-#Route
-@app.route("/chat", methods = ["POST"])
+@app.route("/chat", methods=["POST"])
 def chat():
-
-    request_data = request.get_json()
-    message= request_data["message"]
-
-    data = {}
-    status, response = bot.chat(message)
-    if status == 0:
+    if request.method == "POST":
+        request_data = request.get_json()
+        message = request_data['message']
+        status, response = bot.chat(message)
+        data = {}
         data["reply"] = response
-        data["status"] = 'Success'
-        return jsonify(data)
-    else: 
-        data["reply"] = response
-        data["status"] = 'Failed'
+        data["status"] = 'success' if status == 0 else 'failed'
         return jsonify(data)
 
 
-'''
-#Route
-@app.route("/test", methods = ["POST"])
-def test():
-    request_data = request.get_json()
-    message= request_data["message"]
-    return jsonify(utils.save(message)) #pass
-'''
-
-
-
-
-#app run
 if __name__ == "__main__":
     app.run()
 
